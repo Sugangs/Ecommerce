4e6ce9fdd4ee6386b0cc6d806b24e8b3f678e8b5
Sugangs <135301151+Sugangs@users.noreply.github.com>
2023-10-07 10:20:57 +0530

    Revert "Backend HomeServo"
    This reverts commit 6626d89c22d318c77189916d509e5789e40158a6.

--- HomeServo/src/main/java/com/homeservo/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.homeservo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.homeservo.dto.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-
-}
--- HomeServo/src/main/java/com/homeservo/repository/ServiceCostRepositoty.java
+++
@@ -1,9 +0,0 @@
-package com.homeservo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.homeservo.dto.ServiceCost;
-
-public interface ServiceCostRepositoty extends JpaRepository<ServiceCost, Integer> {
-
-}
